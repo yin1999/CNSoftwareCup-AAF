432799cfdcd0753a8c6cf904185ccf6320c8ca2f
allo <yin199909@aliyun.com>
2020-10-10 18:34:46 +0800

    add remove file

--- main.go
+++ main.go
@@ -301,11 +301,13 @@ func fileReceiver(conn net.Conn, data []byte) error {
 	length := binary.BigEndian.Uint32(data[:4])
 	if _, err = io.CopyN(file, conn, int64(length)); err != nil {
 		file.Close()
+		os.RemoveAll(path)
 		conn.Write(statusErr)
 		return err
 	}
 	file.Close()
 	if err = builder(s); err != nil {
+		os.RemoveAll(path)
 		conn.Write(statusErr)
 		return err
 	}
